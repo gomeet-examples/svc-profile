417b16d21124e27efeb35868aa8738534f3f51f9
Hugues Dubois <hugdubois@gmail.com>
2018-02-16 21:03:32 +0100

    Adding profile model

--- models/models.go
+++ models/models.go
@@ -1,10 +1,26 @@
 package models
 
-// don't forget you need create
-// func <mysql|postgres|sqlite|mssql>Models() (values []interface{}) {}
+import (
+	"time"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+// Profile is the GORM model definition.
+type Profile struct {
+	Uuid      string `gorm:"primary_key;type:char(36);not null"`
+	Gender    uint16
+	Email     string `gorm:"type:varchar(100);unique_index"`
+	Name      string `gorm:"size:255"`
+	Birthday  *time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
+}
 
 // All Mysql models
 func mysqlModels() (values []interface{}) {
-	//values = append(values, &MyModel{})
+	values = append(values, &Profile{})
+
 	return
 }
